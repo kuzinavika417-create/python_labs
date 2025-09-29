2ee9aebf9c64845e43d3ce4761fbe1a6c6cb0500
Вика <kuzinavika417@gmail.com>
2025-09-29 11:32:45 +0300

    lalala

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ print(f'Привет,{name}! Через год тебе будет {age + 1}' )
 
 ![alt text](images/ex01.png)
 
+## Задание 2
+```
+a = input("a: ").replace(',','.')
+b = input("b: ").replace(',','.')
+a = float(a)
+b = float(b)
+summa = a + b
+avg = summa/2
+print(f"sum = {summa:.2f}; avg = {avg:.2f}")
+```
+![alt text](images/ex02.png)
+
--- src/lab01/ex03.py
+++ src/lab01/ex03.py
@@ -1,9 +1,11 @@
-price = input(f"Введите сумму:")
-discount = input(f"Введите сумму:")
-vat = input(f"Введите сумму:")
-base = price*(1 - (discount//100))
-vat_amount = base*(vat//100)
+price = float (input("Цена:"))
+discount = float (input("скидка %:"))
+vat =float (input("НДС%:"))
+
+base = price * (1 - (discount / 100))
+vat_amount = base*(vat / 100)
 total = base + vat_amount
+
 print(f"База после скидки:{base}")
 print(f"НДС:{vat_amount}")
 print(f"Итого к оплате:{total}")
