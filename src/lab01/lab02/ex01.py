356354ea833df55fac9c3a318cafac02b4921c00
Вика <kuzinavika417@gmail.com>
2025-10-12 18:18:14 +0300

    lalala

--- src/lab01/lab02/ex01.py
+++ src/lab01/lab02/ex01.py
@@ -1,13 +1,8 @@
-def min_max(nums: list [float | int]) -> tuple[float|int, float|int]:
-    if not nums:
-        return ValueError    
-    min_val = nums[0]
-    max_val = nums[0]
-    
-    for num in nums [1]:
-        if num < min_val:
-            min_val = num
-        if num > max_val: 
-            max_val = num
-    return min_val, max_val
-print()
+nums = [[3, -1, 5, 5, 0],[42],[-5, -2, -9],[],[1.5, 2, 2.0, -3.1]]
+def min_max(nums):
+    if nums:
+        return min(nums), max(nums)
+    else:
+        return ('ValueError')
+for i in nums:
+    print(f'{i} -> {min_max(i)}')
