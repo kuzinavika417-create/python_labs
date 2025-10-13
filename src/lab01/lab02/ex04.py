aa994d070aefbcd3be40ce46a93a36e47cfc3577
Вика <kuzinavika417@gmail.com>
2025-10-13 20:24:53 +0300

    lalala

---
+++ src/lab01/lab02/ex04.py
@@ -0,0 +1,18 @@
+def transpose(mat: list[list[float | int]]) -> list[list] :
+    if not mat:
+        return []
+    for i in mat :
+        if len(i) != len(mat[0]) :
+            return 'ValueError'
+    str = []
+    for i in range(len(mat[0])) :
+        A = []
+        for j in range(len(mat)):
+            A.append(mat[j][i])
+        str.append(A)
+    return str
+print(f'{[1, 2, 3]} -> {transpose([[1, 2, 3]])}') 
+print(f'{[1], [2], [3]} -> {transpose([[1], [2], [3]])}')  
+print(f'{[[1, 2], [3, 4]]} -> {transpose([[1, 2], [3, 4]])}')  
+print(f'{[]} -> {transpose([])}')  
+print(f'{[[1, 2], [3]]} -> {transpose([[1, 2], [3]])}') 
